fc08f63c84ad1fbd74e8ca4e7ba5ae1f370bcae9
Rafael Abrantes <rafael.vieira.abranttes@gmail.com>
2021-09-18 16:47:15 -0300

    teste

--- v3/layout_vlc/android/app/src/main/kotlin/com/example/layout_vlc/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.layout_vlc
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- v3/layout_vlc/lib/routes/routes.dart
+++
@@ -1,5 +0,0 @@
-class Routes{
-  static final HOME = '/';
-  static final VIDEO_VLC_INTRO = '/video_vlc_intro';
-
-}
