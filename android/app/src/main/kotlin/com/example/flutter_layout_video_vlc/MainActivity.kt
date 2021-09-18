489a635289441ca6c97a22f773aac0cf98d9d225
Rafael Abrantes <rafael.vieira.abranttes@gmail.com>
2021-09-18 16:42:23 -0300

    v3 feita

--- android/app/src/main/kotlin/com/example/flutter_layout_video_vlc/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_layout_video_vlc
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ v3/layout_vlc/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:layout_vlc/home.dart';
+import 'package:layout_vlc/routes/routes.dart';
+import 'package:layout_vlc/vlc_intro/controller/video_player_controller.dart';
+import 'package:layout_vlc/vlc_intro/vlc_screen.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => VideoPlayerControlle(),
+        ),
+      ],
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        routes: {
+          Routes.HOME: (context) => Home(),
+          Routes.VIDEO_VLC_INTRO: (context) => VLCScreen(ModalRoute.of(context).settings.arguments),
+        },
+      ),
+    );
+  }
+}
